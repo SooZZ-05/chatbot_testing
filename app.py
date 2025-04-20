fd667c9ad73ec44e1ba03cd2fa6505bf08332c32
GJW430 <157453927+GJW430@users.noreply.github.com>
2025-04-20 22:46:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -268,8 +268,11 @@ if hf_token and uploaded_file:
     with st.spinner("🔍 Extracting and processing your document..."):
         document_text = extract_text_from_pdf(uploaded_file)
         pdf_chunks = chunk_text(document_text)
-
+        
+if "history" not in st.session_state:
+     st.session_state.history = []
     st.subheader("🧠 Chat with your PDF")
+
     for entry in st.session_state.history:
         with st.chat_message("user"):
             st.markdown(entry["user"])
