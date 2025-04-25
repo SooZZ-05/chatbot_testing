67d55e7fbcb9d7e22113fd5bb5bcdccd10e966fa
MaskedKnightz <163418105+SooZZ-05@users.noreply.github.com>
2025-04-25 11:43:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 
 nltk.download('stopwords')
