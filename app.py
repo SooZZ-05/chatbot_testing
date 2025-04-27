691ddef1276fa7ad6504d5432e4fddcab008b3a9
MaskedKnightz <163418105+SooZZ-05@users.noreply.github.com>
2025-04-27 17:59:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def ask_llm_with_history(question, context, history, api_key):
     messages = [{"role": "system", "content": 
         "You are a friendly AI assistant who gives casual and helpful laptop advice. "
         "Study all the laptop information and ONLY use the internal knowledge you gain from the info belows. "
-        "Respond in a clear, structured format using numbered bullet points for lists, table only for comparision. Each item should start on a new line. "
+        "Respond in a clear, structured format using numbered bullet points for lists. Each item should start on a new line. "
         "Avoid formal tones or sign-offs. Be friendly, clear, and conversational.\n\n"
         f"[INFO SOURCE]\n{context}"}]
 
